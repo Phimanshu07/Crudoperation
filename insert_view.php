682c207aa57a2de439a22f1c6694a017d05648a9
himanshu000007 <himanshuprajapati9768@gmail.com>
2020-07-05 11:51:33 +0530

    first commit

---
+++ insert_view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Insert Option</title>
+</head>
+
+<body>
+  <h2 align="center">Insert Option</h2>
+  <form method="post" action="insert.php" align="center">
+    Option:<input type="text" name="option"></br></br>
+    Type:  <input type="text" name="type"></br></br>
+    Name:  <input type="text" name="name"><br><br>
+    <input type="submit" value="Submit">
+  </form>
+</body>
+
+</html>
